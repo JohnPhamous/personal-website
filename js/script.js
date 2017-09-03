f36142da8cda18a7d272666c7c82c9f74d387112
patricktumbucon <ptumb001@ucr.edu>
2017-09-03 15:07:16 -0700

    removed particle.js demo

--- js/script.js
+++ js/script.js
@@ -4,6 +4,6 @@ $(window).on('load', function() {
   AOS.refresh();
 });
 
-particlesJS.load('particles-js', 'js/particles.js-master/demo/particles.json', function() {
+particlesJS.load('particles-js', 'js/particles.js-master/particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
