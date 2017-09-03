e675e3be38fe33f06d8e82e141fe13cff164f715
patricktumbucon <ptumb001@ucr.edu>
2017-09-03 12:42:36 -0700

    theme edit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 AOS.init();
 
-$(window).on('load', function () {
-    AOS.refresh();
+$(window).on('load', function() {
+  AOS.refresh();
+});
+
+particlesJS.load('particles-js', 'js/particles.js-master/demo/particles.json', function() {
+  console.log('callback - particles.js config loaded');
 });
