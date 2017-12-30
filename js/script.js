f608810f32c817659c6bcc2f0c03f400802bc20a
Patrick Tumbucon <patrick@ucrwpa4-3-104-222.wnet.ucr.edu>
2017-12-29 21:47:38 -0800

    Site redesign

--- js/script.js
+++ js/script.js
@@ -1,9 +1,74 @@
-AOS.init();
+var pathEl = document.querySelector('path');
+var absoluteOffset = anime.timeline();
+var offset = anime.setDashoffset(pathEl);
+pathEl.setAttribute('stroke-dashoffset', offset);
 
-$(window).on('load', function() {
-  AOS.refresh();
-});
+absoluteOffset
+  .add({
+    targets: pathEl,
+    strokeDashoffset: [offset, 0],
+    duration: 750,
+    delay: 300,
+    loop: false,
+    easing: 'easeOutSine',
+    autoplay: true
+  });
 
-particlesJS.load('particles-js', 'js/particles.js-master/particles.json', function() {
-  console.log('callback - particles.js config loaded');
-});
+var polyEls = document.querySelectorAll('polygon');
+
+for (var i = 0; i < polyEls.length; i++) {
+  var polyEl = polyEls[i];
+  var offset = anime.setDashoffset(polyEl);
+  polyEl.setAttribute('stroke-dashoffset', offset);
+
+  absoluteOffset
+    .add({
+      targets: polyEl,
+      strokeDashoffset: [offset, 0],
+      duration: 300,
+      loop: false,
+      easing: 'linear',
+      elasticity: 700,
+      autoplay: true
+    });
+}
+
+absoluteOffset
+  .add({
+    targets: pathEl,
+    duration: 500,
+    stroke: {
+      value: function(el, i) {
+        return 'rgb(220,'+ i * 4 +', 100)';
+      },
+      easing: 'linear',
+      duration: 1000
+    },
+    strokeWidth: {
+      value: 4,
+      easing: 'linear',
+      delay: 200,
+      duration: 600
+    },
+    autoplay: true,
+    direction: 'alternate',
+    loop: true
+  });
+
+  setTimeout(function() {
+    pathEl.setAttribute('stroke', 'rgb(100, 0, 200)');
+    anime({
+      targets: pathEl,
+      stroke: [{
+        value: function(el, i) {
+          return 'rgb(220,'+ i * 2 +', 100)';
+        }},
+        {value: function(el, i) {
+          return 'rgb(100,' + i * 3 +', 200)';
+        }}],
+      easing: 'linear',
+      duration: 3000,
+      direction: 'alternate',
+      loop: true
+    });
+  }, 2800);
