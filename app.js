5e21a7759c51695859066b548251603ffa66c9dd
Patrick Tumbucon <patrick@ucrwpa4-3-104-222.wnet.ucr.edu>
2018-02-07 20:24:33 -0800

    Transitioning to Node

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'))
+});
+
+app.get('/emailsignatures', function(req, res) {
+  res.sendFile(path.join(__dirname + '/views/emailsignatures.html'))
+});
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log('Server started');
+});
+
+
+
